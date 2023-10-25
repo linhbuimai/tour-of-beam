3ca2d4487160689d1c13010ff6ca1863e0adf6a6
Bui Mai Linh <linhmai236@outlook.com>
2023-10-25 12:50:14 +0700

    Consolidate beam example

--- src/exp02_pcollection.py
+++ src/exp02_pcollection.py
@@ -56,6 +56,35 @@ def example_pcollection_read_text():
         )
 
 
+def example_pcollection_read_csv():
+    with beam.Pipeline() as p:
+        lines = (
+            p
+            | "Log lines"
+            >> beam.io.ReadFromText(
+                "gs://apache-beam-samples/nyc_taxi/misc/sample1000.csv"
+            )
+            | beam.ParDo(ExtractTaxiRideCostFn())
+            | beam.combiners.Sample.FixedSizeGlobally(10)
+            | beam.FlatMap(lambda cost: cost)
+            | Output(prefix="Taxi cost: ")
+        )
+
+
+class ExtractTaxiRideCostFn(beam.DoFn):
+    def process(self, element):
+        line = element.split(",")
+        return self.try_parse_taxi_ride_cost(line, 16)
+
+    @classmethod
+    def try_parse_taxi_ride_cost(cls, line, index):
+        if len(line) > index:
+            yield line[index]
+        else:
+            yield 0.0
+
+
 if __name__ == "__main__":
     # example_pcollection_in_memory()
-    example_pcollection_read_text()
+    # example_pcollection_read_text()
+    example_pcollection_read_csv()
